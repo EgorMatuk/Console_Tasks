8d1c07c46e6be212eb1b93a86a57ee1b3dc63a6a
Егор Матюк <egorgorden@gmail.com>
2025-03-23 22:39:35 +0300

    FirstTask

--- Console_Tasks/Program.cs
+++ Console_Tasks/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using static System.Math;
+using static System.Console;
+double Function_z(int z)
+{
+    double result = (1 + 1 / Pow(z,2)) - Sin(z);
+    return result;
+}
+//WriteLine($"First funtion result: {Function_z(3)}");
+
