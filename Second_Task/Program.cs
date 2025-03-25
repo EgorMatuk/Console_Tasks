bdfe47760e50351adcc4d500a021f1a92025854b
Егор Матюк <egorgorden@gmail.com>
2025-03-25 14:01:27 +0300

    Task2_2

--- Second_Task/Program.cs
+++ Second_Task/Program.cs
@@ -5,6 +5,9 @@ using static Methods;
 
 
 //ВАРИАНТ 1
+/* Составьте программу для расчета по двум формулам. Предварительно подготовьте те
+стовые примеры с помощью калькулятора. Отсутствующие в языке функции выразите 
+через имеющиеся.*/ 
 void FindZFunction()
 {
     double a = TheNumEnter("A");
@@ -14,4 +17,14 @@ void FindZFunction()
     WriteLine($"Z1={z1}\nZ2 = {z2}");
 }
 
+
+/*Дано действительное число а. Не пользуясь ни какими другими арифметическими 
+операциями, кроме умножения получить а4 за 2 операции.*/
+void PowFunction(double a)
+{
+    double b = a * a;
+    double result = b * b;
+    WriteLine(result);
+}
 FindZFunction();
+PowFunction(2);
